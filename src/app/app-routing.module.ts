bab86628bb08180e8a4f9e03b575fd585398719c
Samuel Lucas <53665491+samurollie@users.noreply.github.com>
2021-12-12 20:22:19 -0300

    style: :lipstick: Added bootstrap icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+
+
 
 const routes: Routes = [
   {
     path: 'cadastrar',
-    component: CadastroComponent
-  }
+    component: CadastroComponent,
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), NgxBootstrapIconsModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
